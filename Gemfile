943d6cd2ae2223966db4cd47f28df7a2288105ec
TheLe0 <leob.tosin@hotmail.com>
2019-07-13 11:02:41 -0300

    Reformulando projeto

--- Gemfile
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-source 'https://rubygems.org'
-
-gem 'rake'
-gem 'sinatra'
-gem 'sinatra-activerecord'
-gem 'sqlite3'
--- config/enviroment.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-ENV['SINATRA_ENV'] ||= 'development'
-ENV['RACK_ENV'] ||= 'development'
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-require_all 'app'
