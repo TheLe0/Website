8508efc19ce627370e1c35c679398b9d8dc758ff
TheLe0 <leob.tosin@hotmail.com>
2019-07-23 19:58:25 -0300

    Impedir redirecionamento

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const i18n = require('i18n');
+const hbs = require('hbs');
 
 i18n.configure({
-  locales:['en', 'pt-br'],
+  locales:['en', 'pt'],
+  defaultLocale : "en",
   directory: __dirname + '/config/locales'
 });
 
@@ -11,11 +13,19 @@ const porta = process.env.PORT || 8080;
 
 app.set('view engine', 'hbs');
 app.use(express.static('./public'));
+app.use(i18n.init);
 
-app.get('/', (req, res) => {
+hbs.registerHelper('__', function () {
+  return i18n.__.apply(this, arguments);
+});
+hbs.registerHelper('__n', function () {
+  return i18n.__n.apply(this, arguments);
+});
+
+app.get('*', function(req, res){
   res.render('index.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my blog',
+    welcomeMessage: 'Welcome to my Website',
   });
 });
 
