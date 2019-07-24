5d094db1f7c72f99d3ec1d32fc373f05f4ce3cf6
TheLe0 <leob.tosin@hotmail.com>
2019-07-24 19:00:39 -0300

    Adicionado partials

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const porta = process.env.PORT || 8080;
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static('./public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
 app.get('/', function(req, res){
   res.render('index.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my Website',
+    pageTitle: 'Welcome',
+    welcomeMessage: 'Home is where the coffee is',
   });
 });
 
