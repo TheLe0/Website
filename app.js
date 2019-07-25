dac3548ec6dc72517c049b5c0fa4ea3d31a5fabc
TheLe0 <leob.tosin@hotmail.com>
2019-07-25 17:47:15 -0300

    Forcando HTTPS

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const hbs = require('hbs');
 const app = express();
 const porta = process.env.PORT || 8080;
 
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static('./public'));
+app.use(requireHTTPS);
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
